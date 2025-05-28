6d9b2c31de9a946950a9a5670d44c45fd652ef92
João Pedro <devcontatojoao@gmail.com>
2025-05-28 09:37:56 -0300

    chore: Update do clean database

--- Database.cs
+++ Database.cs
@@ -2,38 +2,23 @@
 
 using BankManager.Model;
 
-//public static class Database
-//{
-//    public static List<BankModel> Banks = new();
-//    public static List<ClientModel> Clients = new();
-//}
+public static class Database
+{
+    public static List<BankModel> Banks = new();
+    public static List<ClientModel> Clients = new();
+}
 
 //public static class Database
 //{
 //    public static List<BankModel> Banks = new() {
-//        new BankModel() {Id = Guid.NewGuid().ToString().Substring(0,8), Name = "Santander" },
-//        new BankModel() {Id = Guid.NewGuid().ToString().Substring(0,8), Name = "Itaú" },
-//        new BankModel() {Id = Guid.NewGuid().ToString().Substring(0,8), Name = "Bradesco" }
+//        new BankModel() {Id = Utils.GenerateId(), Name = "Santander" },
+//        new BankModel() {Id = Utils.GenerateId(), Name = "Itaú" },
+//        new BankModel() {Id = Utils.GenerateId(), Name = "Bradesco" }
 
 //    };
 
 //    public static List<ClientModel> Clients = new() {
-//        new ClientModel() { Id = Guid.NewGuid().ToString().Substring(0,8), Name = "Ana", Bank = Banks[0] } ,
-//        new ClientModel() { Id = Guid.NewGuid().ToString().Substring(0,8), Name = "João", Bank = Banks[1] }
+//        new ClientModel() { Id = Utils.GenerateId(), Name = "Ana", Bank = Banks[0] } ,
+//        new ClientModel() { Id = Utils.GenerateId(), Name = "João", Bank = Banks[1] }
 //    };
 //}
-
-public static class Database
-{
-    public static List<BankModel> Banks = new() {
-        new BankModel() {Id = Utils.GenerateId(), Name = "Santander" },
-        new BankModel() {Id = Utils.GenerateId(), Name = "Itaú" },
-        new BankModel() {Id = Utils.GenerateId(), Name = "Bradesco" }
-
-    };
-
-    public static List<ClientModel> Clients = new() {
-        new ClientModel() { Id = Utils.GenerateId(), Name = "Ana", Bank = Banks[0] } ,
-        new ClientModel() { Id = Utils.GenerateId(), Name = "João", Bank = Banks[1] }
-    };
-}
